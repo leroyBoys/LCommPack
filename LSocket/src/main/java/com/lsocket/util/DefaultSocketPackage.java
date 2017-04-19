87c72206c6132c844647b6a282916d43c2049a39
leroyBoys <wlvxiaohui@163.com>
2017-04-19 18:10:17 +0800

    ssssssssssss

--- LSocket/src/main/java/com/lsocket/util/DefaultSocketPackage.java
+++ LSocket/src/main/java/com/lsocket/util/DefaultSocketPackage.java
@@ -8,14 +8,14 @@ import org.apache.mina.core.session.IoSession;
  * 默认传输包协议为：1byte或者2byte,3byte,5byte
  * \-PackageType(1byte)-\-数据长度(1-2byte)-\-module(1byte不能大于256)-\-cmd(1byte不能大于256)-\
  * PackageType(1byte)解析如下
- * 1位：是否是心跳；2位：压缩方式（0-4）;
+ * 1位：是否是心跳；2位：加密方式（0-4）;
  * 1位：是否包含cmd和module;1位：是否是大数据（如果是大数据则长度展2字节，否则1字节）；3位：class类型（0-8）
  * 数据长度最大值为256*?+?=max=65792( 最大包支持64K)
  * Created by leroy:656515489@qq.com
  * 2017/4/6.
  */
 public class DefaultSocketPackage {
-    private final static int compresseMinLength = 150;
+    private final static int compresseMinLength = 200;
 
     /**
      * 反解析
@@ -31,8 +31,8 @@ public class DefaultSocketPackage {
                 return;
             }
 
-            int compreType = header>>PackageType.compresseType&3;
-            System.out.println("compreType:"+compreType);
+            int encryType = header>>PackageType.encryType&3;
+            System.out.println("compreType:"+encryType);
             int isContainCmdModule = header>>PackageType.cmdModuleContain&1;
             System.out.println("isContainCmdModule:"+isContainCmdModule);
             int isBiggerData = header>>PackageType.isBiggerData&1;
@@ -60,17 +60,17 @@ public class DefaultSocketPackage {
         return null;
     }
 
-    public static IoBuffer transformErrorMsg(byte[] bytes,int compressType) {
+    public static IoBuffer transformErrorMsg(byte[] bytes,int encryType) {
         try {
            final int messageLength = bytes.length;
             IoBuffer buffer;
             if(messageLength>'\uffff'){
-                byte packageType = (byte) (0|(compressType<<PackageType.compresseType)|0|1<<PackageType.isBiggerData|(1<<PackageType.classType));
+                byte packageType = (byte) (0|(encryType<<PackageType.encryType)|0|1<<PackageType.isBiggerData|(1<<PackageType.classType));
                 buffer = IoBuffer.allocate(messageLength + 3);
                 buffer.put(packageType);
                 buffer.putShort((short) messageLength);
             }else {
-                byte packageType = (byte) (0|(compressType<<PackageType.compresseType)|0|0|(1<<PackageType.classType));
+                byte packageType = (byte) (0|(encryType<<PackageType.encryType)|0|0|(1<<PackageType.classType));
                 buffer = IoBuffer.allocate(messageLength + 2);
                 buffer.put(packageType);
                 buffer.put((byte) messageLength);
@@ -85,18 +85,18 @@ public class DefaultSocketPackage {
         return null;
     }
 
-    public static IoBuffer transformByteArray(byte[] bytes,int compressType,byte module,byte cmd) {
+    public static IoBuffer transformByteArray(byte[] bytes,int encryType,byte module,byte cmd) {
         try {
             final int messageLength = bytes.length;
 
             IoBuffer buffer;
             if(messageLength>'\uffff'){
-                byte packageType = (byte) (0|(compressType<<PackageType.compresseType)|1<<PackageType.cmdModuleContain|1<<PackageType.isBiggerData|0);
+                byte packageType = (byte) (0|(encryType<<PackageType.encryType)|1<<PackageType.cmdModuleContain|1<<PackageType.isBiggerData|0);
                 buffer = IoBuffer.allocate(messageLength + 5);
                 buffer.put(packageType);
                 buffer.putShort((short) messageLength);
             }else {
-                byte packageType = (byte) (0|(compressType<<PackageType.compresseType)|1<<PackageType.cmdModuleContain|0|0);
+                byte packageType = (byte) (0|(encryType<<PackageType.encryType)|1<<PackageType.cmdModuleContain|0|0);
                 buffer = IoBuffer.allocate(messageLength + 4);
                 buffer.put(packageType);
                 buffer.put((byte) messageLength);
@@ -140,7 +140,7 @@ public class DefaultSocketPackage {
                 return receiveData;
             }
 
-            int compreType = header>>PackageType.compresseType&3;
+            int compreType = header>>PackageType.encryType&3;
             if(compreType > 3 || compreType < 0){
                 receiveData.close();
                 SystemLogger.error(DefaultSocketPackage.class,"compreType:"+compreType+" not right"+ session.toString());
@@ -202,8 +202,9 @@ public class DefaultSocketPackage {
     final class PackageType{
 
         private static final int heart = 0;
-        private static final int compresseType = 1+heart;
-        private static final int cmdModuleContain = 2+compresseType;
+        /** 加密方式encry */
+        private static final int encryType = 1+heart;
+        private static final int cmdModuleContain = 2+encryType;
         private static final int isBiggerData = 1+cmdModuleContain;
         private static final int classType = 1+isBiggerData;
     }
