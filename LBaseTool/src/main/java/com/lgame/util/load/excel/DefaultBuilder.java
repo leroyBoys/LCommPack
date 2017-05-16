168cc84ce6029a69cbbf688fbc0caa3a2bff853e
leroyBoys <wlvxiaohui@163.com>
2017-05-16 22:57:09 +0800

    sddsdf

--- LBaseTool/src/main/java/com/lgame/util/load/excel/DefaultBuilder.java
+++ LBaseTool/src/main/java/com/lgame/util/load/excel/DefaultBuilder.java
@@ -11,72 +11,78 @@ import java.lang.reflect.Modifier;
 import java.util.*;
 
 public class DefaultBuilder implements IBuilder {
-	private DefaultBuilder(){}
-	private static final DefaultBuilder obj = new DefaultBuilder();
+    private DefaultBuilder() {
+    }
 
-	public final static DefaultBuilder getInstance(){
-		return obj;
-	}
+    private static final DefaultBuilder obj = new DefaultBuilder();
+
+    public final static DefaultBuilder getInstance() {
+        return obj;
+    }
 
 
-	@Override
-	public <B> Iterator<B> initBuild(String[][] data, Class<B> beanClass,int dataFromLine) throws Exception {
+    @Override
+    public <B> Iterator<B> initBuild(String[][] data, Class<B> beanClass, int dataFromLine) throws Exception {
 
-		Field[] fieldMap = processProperties(beanClass,data[0]);
-		String[] row;
-		B b;
-		List<B> list = new LinkedList<>();
-		for (int i = dataFromLine; i < data.length; i++) {
-			row = data[i];
-			b = beanClass.newInstance();
-			initBean(b,row,fieldMap);
-			list.add(b);
-		}
+        Field[] fieldMap = processProperties(beanClass, data[0]);
+        String[] row;
+        B b;
+        List<B> list = new LinkedList<>();
+        for (int i = dataFromLine; i < data.length; i++) {
+            row = data[i];
+            b = beanClass.newInstance();
+            initBean(b, row, fieldMap);
+            list.add(b);
+        }
 
-		return list.iterator();
-	}
+        return list.iterator();
+    }
 
-	private <B> void initBean(B b, String[] row,Field[] head) {
+    private <B> void initBean(B b, String[] row, Field[] head) {
 
-		for(int i = 0;i< head.length;i++){
-				Object transformValue = transformerFieldValue(row[i], head[i]);
-				try {
-					head[i].set(b, transformValue);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-		}
-	}
+        for (int i = 0; i < head.length; i++) {
+            if (head[i] == null) {
+                continue;
+            }
 
-	public static Object transformerFieldValue(String value, Field field) {
-		try {
-			value = value.trim().replaceAll("\\n","");
+            Object transformValue = transformerFieldValue(row[i], head[i]);
+            try {
+                head[i].set(b, transformValue);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-			Class<?> fieldType = field.getType();
-			Class<? extends PropertyTransformer> formers = PropertyTransformer.class;
-			PropertyTransformer<?> propertyTransformer = PropertyTransformerFactory.newTransformer(fieldType, formers);
-			return propertyTransformer.transform(value, field);
-		} catch (Exception e) {
-			System.out.println(JsonTool.getJsonFromBean(field)+" --->:"+value+"  formatData is error");
-		}
-		PrintTool.info(field.getName()+" = "+value);
-		return null;
-	}
+    public static Object transformerFieldValue(String value, Field field) {
+        try {
+            value = value.trim().replaceAll("\\n", "");
 
-	public static Field[] processProperties(Class<?> clazz,String[] head) {
-		Map<String, Field> bundleMaps = new HashMap();
-		for (Field field : clazz.getDeclaredFields()) {
-				if (!Modifier.isPublic(field.getModifiers())) {
-					field.setAccessible(true);
-				}
-				bundleMaps.put(field.getName(),field);
-			}
-			Field[] ret = new Field[head.length];
-		    for(int i = 0;i<head.length;i++){
-				ret[i] = bundleMaps.get(head[i]);
-			}
+            Class<?> fieldType = field.getType();
+            Class<? extends PropertyTransformer> formers = PropertyTransformer.class;
+            PropertyTransformer<?> propertyTransformer = PropertyTransformerFactory.newTransformer(fieldType, formers);
+            return propertyTransformer.transform(value, field);
+        } catch (Exception e) {
+            System.out.println(JsonTool.getJsonFromBean(field) + " --->:" + value + "  formatData is error");
+        }
+        PrintTool.info(field.getName() + " = " + value);
+        return null;
+    }
 
-			return ret;
-	}
+    public static Field[] processProperties(Class<?> clazz, String[] head) {
+        Map<String, Field> bundleMaps = new HashMap();
+        for (Field field : clazz.getDeclaredFields()) {
+            if (!Modifier.isPublic(field.getModifiers())) {
+                field.setAccessible(true);
+            }
+            bundleMaps.put(field.getName(), field);
+        }
+        Field[] ret = new Field[head.length];
+        for (int i = 0; i < head.length; i++) {
+            ret[i] = bundleMaps.get(head[i]);
+        }
+
+        return ret;
+    }
 }
 
